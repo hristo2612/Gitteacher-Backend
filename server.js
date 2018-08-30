676105584ef8b2204580a8dc34ec706686c7d691
hristo2612 <hristoapps@gmail.com>
2018-08-31 01:32:30 +0300

    Barebones for server

--- server.js
+++ server.js
@@ -3,12 +3,71 @@ const fs = require('fs');
 const http = require('http');
 const path = require("path");
 const bodyParser = require('body-parser');
+const session = require('express-session');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const errorhandler = require('errorhandler');
+const connectMongo = require('connect-mongo')(session);
+const morgan = require('morgan');
+const methodOverride = require('method-override');
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+const mongoProdUrl = process.env.MONGODB_URI + '/gittus';
+const mongoDevUrl = 'mongodb://localhost/gittus';
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(express.static(path.resolve(__dirname, '/public')));
 
+if (isProduction) {
+  mongoose.connect(mongoProdUrl, { useNewUrlParser: true });
+} else {
+  mongoose.connect(mongoDevUrl, { useNewUrlParser: true });
+  mongoose.set('debug', true);
+}
+
+app.use(session({ store: new connectMongo({ mongooseConnection: mongoose.connection }), secret: 'gittus', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false }));
+
+app.get('/', (req, res) => {
+  res.send('Hi');
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.use((req, res, next) => {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if (!isProduction) {
+  app.use((err, req, res, next) => {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({
+      'errors': {
+        message: err.message,
+        error: err
+      }
+    });
+  });
+}
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    'errors': {
+      message: err.message,
+      error: {}
+    }
+  });
+});
+
+app.listen(process.env.PORT || 3000, () => console.log('Listening on port 3000!'))

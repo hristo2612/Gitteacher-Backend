2def9a355b3fafe1e16588360b79df6ba3195963
hristo2612 <hristoapps@gmail.com>
2018-09-08 01:56:44 +0300

    Cleanup unused files

--- git/repo/getRepo.js
+++
@@ -1,9 +0,0 @@
-var nodegit = require("nodegit");
-var projectRoot = require("../../constants").projectRoot;
-
-var path = require("path");
-
-module.exports = function(repo) {
-    repo = repo ? repo : "./fb-event-stats";
-    return nodegit.Repository.open(path.resolve(projectRoot, repo));
-};

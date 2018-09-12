a79ae01f209a94bc6795c99b10b502ce64cf3ef7
hristo2612 <hristoapps@gmail.com>
2018-09-12 20:17:23 +0300

    Add api docs as main path of backend

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,9 @@
 const router = require("express").Router();
+const path = require("path");
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../README.md"));
+});
 
 router.use("/users", require("./users"));
 
